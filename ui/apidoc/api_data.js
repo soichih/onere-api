46e3c246625bf2f299463285c586107496a3e3c9
Soichi Hayashi <soichih@gmail.com>
2016-08-08 14:55:50 +0000

    adding apidoc

---
+++ ui/apidoc/api_data.js
@@ -0,0 +1,153 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "/health",
+    "title": "Get current service status",
+    "name": "Health",
+    "group": "System",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>'ok' or 'failed'</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "./controllers.js",
+    "groupTitle": "System"
+  },
+  {
+    "type": "get",
+    "url": "/entry",
+    "title": "Query Registrations",
+    "description": "<p>Query registered ONORE entrires</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Object",
+            "optional": false,
+            "field": "find",
+            "description": "<p>Optional Mongo find query - defaults to {}</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Object",
+            "optional": false,
+            "field": "sort",
+            "description": "<p>Optional Mongo sort object - defaults to {}</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "select",
+            "description": "<p>Optional Fields to load - defaults to 'logical_id'</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "limit",
+            "description": "<p>Optional Maximum number of records to return - defaults to 100</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "skip",
+            "description": "<p>Optional Record offset for pagination</p>"
+          }
+        ]
+      }
+    },
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "authorization",
+            "description": "<p>A valid JWT token &quot;Bearer: xxxxx&quot;</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "resources",
+            "description": "<p>Resource detail</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "./controllers.js",
+    "group": "_usr_local_git_onore_api_controllers_js",
+    "groupTitle": "_usr_local_git_onore_api_controllers_js",
+    "name": "GetEntry"
+  },
+  {
+    "type": "get",
+    "url": "/stage",
+    "title": "Request to stage ODI exposures",
+    "description": "<p>From a list of exposure_ids, it makes sure user has access to it, find where the data is (on disk, sda, etc..) TODO - thaw them if necessary and</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String[]",
+            "optional": false,
+            "field": "exposure_ids",
+            "description": "<p>List of exposure IDs to thaw / stage</p>"
+          }
+        ]
+      }
+    },
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "authorization",
+            "description": "<p>A valid JWT token &quot;Bearer: xxxxx&quot;</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "exposures",
+            "description": "<p>staged</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "./controllers.js",
+    "group": "_usr_local_git_onore_api_controllers_js",
+    "groupTitle": "_usr_local_git_onore_api_controllers_js",
+    "name": "GetStage"
+  }
+] });
---
+++ ui/apidoc/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "odi",
+  "version": "1.0.0",
+  "description": "ODI REST API server",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-08-08T14:55:34.550Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
